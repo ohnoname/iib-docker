f39acdef0c4bab86561948c3762e27ac3331d232
Joerg Wende <jwende@gmx.de>
2016-07-29 12:01:06 +0200

    Refactor 5

--- iib_manage.sh
+++ iib_manage.sh
@@ -5,7 +5,9 @@
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-
+echo "----------------------------------"
+echo "Version 18"
+echo "----------------------------------"
 set -e
 
 NODE_NAME=${NODENAME-IIBV10NODE}
